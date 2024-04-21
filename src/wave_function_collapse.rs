8fb5b0d050461f4a16d68172dbc143263f3660fe
Marcus Elg <melg@kth.se>
2024-04-21 10:10:08 +0200

    Expose weight to config

--- src/wave_function_collapse.rs
+++ src/wave_function_collapse.rs
@@ -7,13 +7,7 @@ use std::{
 use anyhow::{bail, ensure};
 use nalgebra::{Rotation3, Vector2};
 
-use crate::{
-    model_loader::ModelLoader,
-    scene::Scene,
-    tile::{self, Tile},
-    tile_data::TileData,
-    Direction,
-};
+use crate::{model_loader::ModelLoader, scene::Scene, tile::Tile, tile_data::TileData, Direction};
 
 pub struct WFC<'a> {
     map_size: usize,
@@ -91,28 +85,41 @@ impl<'a> WFC<'a> {
                                 .map(|s| s.to_string())
                                 .collect::<Vec<String>>();
                             ensure!(
-                                values.len() == 6,
+                                values.len() == 7,
                                 format!(
                                     "Line {} \"{}\" does not contain all values required",
                                     index + 1,
                                     line
                                 )
                             );
+                            let weight = values[1].parse::<u32>();
                             ensure!(
-                                values[5] == "true" || values[5] == "false",
+                                weight.is_ok() && weight.clone().unwrap() > 0,
                                 format!(
-                                    "On line {} the rotatable value can only be true or false",
+                                    "On line {} the weight value can only be a non-negative integer",
+                                    index + 1
+                                )
+                            );
+                            let weight = weight.unwrap();
+                            ensure!(
+                                values[6] == "1" || values[6] == "2" || values[6] == "4",
+                                format!(
+                                    "On line {} the rotatable value can only be 1, 2 or 4",
                                     index + 1
                                 )
                             );
 
                             // Load models
-                            let rotation_angles: Vec<Rotation3<f32>> = match values[5].as_str() {
-                                "true" => vec![
+                            let rotation_angles: Vec<Rotation3<f32>> = match values[6].as_str() {
+                                "4" => vec![
                                     Rotation3::from_euler_angles(0.0, 0.0, 0.0),
                                     Rotation3::from_euler_angles(0.0, 0.0, PI / 2.0),
                                     Rotation3::from_euler_angles(0.0, 0.0, PI),
                                     Rotation3::from_euler_angles(0.0, 0.0, 3.0 / 2.0 * PI),
+                                ],
+                                "2" => vec![
+                                    Rotation3::from_euler_angles(0.0, 0.0, 0.0),
+                                    Rotation3::from_euler_angles(0.0, 0.0, PI / 2.0),
                                 ],
                                 _ => vec![Rotation3::from_euler_angles(0.0, 0.0, 0.0)],
                             };
@@ -125,11 +132,11 @@ impl<'a> WFC<'a> {
                                     Ok(model) => {
                                         let tile = TileData {
                                             model,
-                                            weight: 1, // TODO: expose to config
-                                            up_edge: values[1 + (index % 4)].clone(),
-                                            right_edge: values[1 + ((index + 1) % 4)].clone(),
-                                            down_edge: values[1 + ((index + 2) % 4)].clone(),
-                                            left_edge: values[1 + ((index + 3) % 4)].clone(),
+                                            weight,
+                                            up_edge: values[2 + (index % 4)].clone(),
+                                            right_edge: values[2 + ((index + 1) % 4)].clone(),
+                                            down_edge: values[2 + ((index + 2) % 4)].clone(),
+                                            left_edge: values[2 + ((index + 3) % 4)].clone(),
                                         };
                                         tiles.push(tile);
                                     }
@@ -163,3 +170,6 @@ impl<'a> WFC<'a> {
             && index.y < self.map_size as i32
     }
 }
+
+// Determines the order of tile collapsing
+struct PlacementStrategy {}
